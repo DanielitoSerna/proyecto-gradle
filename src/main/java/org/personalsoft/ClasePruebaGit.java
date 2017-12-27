48b037f8d8f26f8263f95b3e8452ee6913ed816b
Daniel Serna <danielestebanse@gmail.com>
2017-12-27 13:42:47 -0500

    Se agregan archivos para prueba de gradle

---
+++ src/main/java/org/personalsoft/ClasePruebaGit.java
@@ -0,0 +1,17 @@
+package org.personalsoft;
+
+public class ClasePruebaGit {
+
+	private final int number;
+
+	public ClasePruebaGit(int number) {
+		this.number = number;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+	
+	
+	
+}
